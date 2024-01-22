4e73e3f41a2e827c374a6561c8143498dc427f62
LeeDongJun <djlee1995@daum.net>
2024-01-22 21:54:03 +0900

    Reserve-Server, Controller 생성 및 Config Bean 등록

---
+++ Reserve-Server/src/main/java/org/spring/reserve/ReserveServerApplication.java
@@ -0,0 +1,13 @@
+package org.spring.reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveServerApplication.class, args);
+    }
+
+}
---
+++ Reserve-Server/src/test/java/org/spring/reserve/ReserveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
