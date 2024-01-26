6582b9dd0d7b141f1049db492430e019c713b946
LeeDongJun <djlee1995@daum.net>
2024-01-26 21:50:46 +0900

    Reserve-Server, JWT 테스트 컨트롤러 생성

--- Reserve-Server/src/main/java/org/spring/reserve/controller/AdminController.java
+++ Reserve-Server/src/main/java/org/spring/reserve/controller/AdminController.java
@@ -1,16 +1,30 @@
 package org.spring.reserve.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
 public class AdminController {
 
-    @GetMapping("/admin")
+    @GetMapping("/api/admin/admin")
     public String adminP() {
+        String memberId = SecurityContextHolder.getContext().getAuthentication().getName();
 
-        return "admin Controller";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "admin Controller" +memberId +role;
     }
 }
